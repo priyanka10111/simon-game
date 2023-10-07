027e53292300e9ab8749050757875b52ada07678
priyanka10111 <143818732+priyanka10111@users.noreply.github.com>
2023-10-07 15:13:54 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,12 @@
+
+var buttonColors = ["red" , "blue " , "green" ,"yellow"];
+ var gamePattern = [];
+
+function nextSequence(){
+    var randomNumber = Math.floor (Math.random() * 4) ;
+   }
+
+
+var RandomChosenColor = buttonColors[randomNumber];
+ gamePattern.push(RandomChosenColor);
+ 
